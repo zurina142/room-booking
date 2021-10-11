659b776c55d15cf6a4de352c644e67cff126f07c
zurina142 <zurina.rahman@gmail.com>
2021-10-11 14:50:07 +0800

    add room

---
+++ database/migrations/2021_10_11_050250_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('capacity')->nullable()->comment('Jumlah kapasiti peserta dibenarkan mengikut kategori bilik');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('room_category_id')->unsigned()->nullable();
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
